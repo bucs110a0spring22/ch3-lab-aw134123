54ea36afc1f96f39ea4496e06a168e017bef43d6
Andrew Wang <98830902+aw134123@users.noreply.github.com>
2022-02-22 14:57:50 +0000

    Gave up on making it fancy

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ Turtles race first with one random step and then with ten random steps. Then it goes to the middle and draws some shapes
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ I wasn't able to make it fancier
 
 ## REFERENCES:
- < List any outside resources used >
+ [](https://stackoverflow.com/questions/46091442/how-can-i-make-the-turtle-a-random-color)
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+no
--- main.py
+++ main.py
@@ -1,26 +1,83 @@
 import turtle               #1. import modules
 import random
+import time
 
 #Part A
 window = turtle.Screen()       # 2.  Create a screen
 window.bgcolor('lightblue')
 
-michelangelo = turtle.Turtle()    # 3.  Create two turtles
+# 3.  Create two turtles
+michelangelo = turtle.Turtle()    
 leonardo = turtle.Turtle()
 michelangelo.color('orange')
 leonardo.color('blue')
 michelangelo.shape('turtle')
 leonardo.shape('turtle')
 
-michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
+# 4.  Pick up the pen so we don’t get lines
+michelangelo.up()          
 leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. your code goes here
+michelangelo.down()
+leonardo.down()
+michelangelo.forward(random.randint(0,100))
+leonardo.forward(random.randint(0,100))
+time.sleep(1)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+michelangelo.clear()
+leonardo.clear()
+
+for turtle_step in range(0,10):
+  michelangelo.forward(random.randint(0,10))
+  leonardo.forward(random.randint(0,10))
+  time.sleep(0.3)
+time.sleep(.5)
+michelangelo.goto(0,0)
+leonardo.goto(0,0)
+michelangelo.clear()
+michelangelo.hideturtle()
+leonardo.clear()
 
 
-# Part B - complete part B here
+#Part B - complete part B here
+# van_gogh = turtle.Turtle()
+# rembrandt = turtle.Turtle()
+# van_gogh.color('yellow')
+# rembrandt.color('brown')
+# van_gogh.shape('turtle')
+# rembrandt.shape('turtle')
+# turtle_roster = [michelangelo, leonardo, van_gogh, rembrandt]
+# turtle_killed = random.choice(turtle_roster)
+# turtle_roster.remove(turtle_killed)
+# turtle_spared = random.choice(turtle_roster)
+# def change_color():
+#     #R = random.random()
+#     #B = random.random()
+#     #G = random.random()
+
+#     #turtle.color(R, G, B)
+# turtle_spared.color()
+# turtle_spared.shae('turtle')
+
+
+for side in [3, 4 , 6, 9, 12]:
+  side_length = random.randint(10, 50)
+ 
+  for i in range (0, side):
+    leonardo.forward(side_length)
+    leonardo.right(360/side)
+    
+  
+  
+  leonardo.clear()  
+ 
+  
+  
+  
 
 
 window.exitonclick()
